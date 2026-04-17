4290dcb7564197970fd98a358f487a6e7573a838
musdalifahzahra <124250135@student.upnyk.ac.id>
2026-04-17 10:11:58 +0700

    catatTransaksi - ubah perintah sql di php jadi fungsi di koneksi

---
+++ .history/0-koneksi_20260417100504.php
@@ -0,0 +1,46 @@
+<?php
+//koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "laporan_keuangan");
+
+//ambil data 
+$result = mysqli_query($conn, "SELECT * FROM transaksi");
+if (!$result) {
+    echo mysqli_error($conn);
+}
+
+//FUNGSI UNTUK MENAMBAHKAN DATA (INSERT)
+function tambah_data($data_insert)
+{
+
+    global $conn;
+    $tanggal = $data_insert["tanggal"];
+    $keterangan = $data_insert["keterangan"];
+    $kategori = $data_insert["kategori"];
+    $jenis = $data_insert["jenis"];
+    $jumlah = $data_insert["jumlah"];
+    $catatan = $data_insert["catatan"];
+
+    $insert = "INSERT INTO transaksi
+              VALUES (null, '$tanggal', '$keterangan', '$kategori', '$jenis', '$jumlah', '$catatan')
+              ";
+
+    mysqli_query($conn, $insert);
+
+    //mengembalikan nilai, cek berhasil apa ngga. kalo (1) = berhasil, (-1)= tidak berhasil;
+    return mysqli_affected_rows($conn);
+}
+
+// FUNGSI UNTUK BACA DATA DARI DATABASE
+function nampilin_data($query)
+{
+    global $conn;
+
+    $result = mysqli_query($conn, $query);
+    //$rows = wadah untuk menyimpan baris-baris yg akan di ambil 
+    $rows = [];
+    while ($row = mysqli_fetch_rows($result)) {
+        $rows[] = $row;
+    }
+    // mengmebalikan semua baris-baris($row) yg uda kesimpan di $rows
+    return $rows;
+}
---
+++ .history/koneksi_20260417081450.php
@@ -0,0 +1,17 @@
+<?php
+//koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "laporan_keuangan");
+
+//ambil data 
+$result = mysqli_query($conn, "SELECT * FROM transaksi");
+var_dump($result);
+if (!$result) {
+    echo mysqli_error($conn);
+}
+
+//FUNGSI UNTUK MENAMBAHKAN DATA (INSERT)
+
+
+
+
+?>
