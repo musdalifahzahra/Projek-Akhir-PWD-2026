69313d26bf7933835a48d8ed731e3bd535ffdbda
musdalifahzahra <124250135@student.upnyk.ac.id>
2026-05-21 21:50:17 +0700

    perbaiki

--- 3-catat-transaksi.php
+++ 3-catat-transaksi.php
@@ -42,7 +42,7 @@ if (isset($_POST["submit"])) {
                 <path d="M17 14h.01" />
                 <path d="M7 7h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14" />
             </svg>
-            <span>Sembako Makmur</span>
+            <span><?= $_SESSION["nama_toko"] ?></span>
         </div>
 
         <div class="nav-kanan">
